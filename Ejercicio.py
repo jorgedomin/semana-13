e90bee4617a2e931e6707d62b101dcfe484df456
jorgedomin <142687225+jorgedomin@users.noreply.github.com>
2024-03-13 22:10:10 -0500

    Ejercicio.py

--- Ejercicio.py
+++ Ejercicio.py
@@ -6,7 +6,7 @@ class AplicacionGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Aplicación GUI Básica")
-        self.root.geometry("300x300")  # Ajusta el tamaño de la ventana a 300x300
+        self.root.geometry("300x300")  # Ajusta el tamaño de la ventana a 900x900
 
         # Crear menú
         self.menu = tk.Menu(self.root)
@@ -16,7 +16,7 @@ class AplicacionGUI:
         self.archivo_menu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="Archivo", menu=self.archivo_menu)
         self.archivo_menu.add_command(label="Agregar", command=self.agregar)
-        self.archivo_menu.add_command(label="Limpiar", command=self.limpiar)
+        self.archivo_menu.add_command(label="Limpiar o Borar", command=self.limpiar)
         self.archivo_menu.add_separator()
         self.archivo_menu.add_command(label="Salir", command=self.root.quit)
 
@@ -30,7 +30,7 @@ class AplicacionGUI:
         self.boton_agregar = tk.Button(root, text="Agregar", command=self.agregar)
         self.boton_agregar.pack()
 
-        self.boton_limpiar = tk.Button(root, text="Limpiar", command=self.limpiar)
+        self.boton_limpiar = tk.Button(root, text="Limpiar o Borar", command=self.limpiar)
         self.boton_limpiar.pack()
 
         self.lista = tk.Listbox(root)
